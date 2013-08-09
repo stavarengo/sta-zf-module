475621f2adfef9a7aede02f52dc607937584bfd3
Stavarengo <faelsta@gmail.com>
2013-08-09 18:08:52 -0300

    atualizando

--- src/Sta/View/Helper/IsMobile.php
+++ src/Sta/View/Helper/IsMobile.php
@@ -1,10 +1,17 @@
 <?php
 namespace Sta\View\Helper;
 
+use Zend\ServiceManager\ServiceLocatorAwareInterface;
+use Zend\ServiceManager\ServiceLocatorInterface;
 use Zend\View\Helper\AbstractHelper;
 
-class IsMobile extends AbstractHelper
+class IsMobile extends AbstractHelper implements ServiceLocatorAwareInterface
 {
+	/**
+	 * @var ServiceLocatorAwareInterface
+	 */
+	private $serviceLocator;
+	
 	/**
 	 * @var bool
 	 */
@@ -17,7 +24,7 @@ class IsMobile extends AbstractHelper
 
 	public function __invoke()
 	{
-		if ($this->isMobile === null) {
+		if ($this->isMobile === null && !($this->isMobile = $this->getServiceLocator()->getServiceLocator()->get('isDebug'))) {
 			require_once __DIR__ . '/../../../../../../vendor/Mobile-Detect/Mobile_Detect.php';
 			$this->detect   = new \Mobile_Detect();
 			$this->isMobile = $this->detect->isMobile();
@@ -25,4 +32,23 @@ class IsMobile extends AbstractHelper
 		return $this->isMobile;
 	}
 
+	/**
+	 * Set service locator
+	 *
+	 * @param ServiceLocatorInterface $serviceLocator
+	 */
+	public function setServiceLocator(ServiceLocatorInterface $serviceLocator)
+	{
+		$this->serviceLocator = $serviceLocator;
+	}
+
+	/**
+	 * Get service locator
+	 *
+	 * @return ServiceLocatorInterface
+	 */
+	public function getServiceLocator()
+	{
+		return $this->serviceLocator;
+	}
 }

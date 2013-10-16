4dc09449a226e4abc4f2a4b2a06ca25ca9c8a202
Stavarengo <faelsta@gmail.com>
2013-10-16 15:46:35 -0300

    aprimorado validador

--- src/Sta/Entity/Validator/RegistroDuplicado.php
+++ src/Sta/Entity/Validator/RegistroDuplicado.php
@@ -17,6 +17,22 @@ class RegistroDuplicado extends \Zend\Validator\AbstractValidator
 	protected $messageTemplates = array(
 		self::ENTIDADE_DUPLICADA => "Registro duplicado",
 	);
+	/**
+	 * @var RegistroDuplicadoValue
+	 */
+	protected $value;
+	/**
+	 * @var $em EntityManager
+	 */
+	private $em;
+	/**
+	 * @var AbstractEntity
+	 */
+	private $entity;
+	/**
+	 * @var string
+	 */
+	private $entityClass;
 
 	/**
 	 * @param RegistroDuplicadoValue $value
@@ -30,19 +46,89 @@ class RegistroDuplicado extends \Zend\Validator\AbstractValidator
 			throw new \Sta\Entity\Validator\InvalidArgument();
 		}
 
-		/** @var $em EntityManager */
-		$em               = \Sta\Module::getServiceLocator()->get('Doctrine\ORM\EntityManager');
-		$entity           = $value->entity;
-		$entityClass      = get_class($entity);
-		$entityRepository = $em->getRepository($entityClass);
-		$qb = $entityRepository->createQueryBuilder('a');
+		$this->em          = \Sta\Module::getServiceLocator()->get('Doctrine\ORM\EntityManager');
+		$this->value       = $value;
+		$this->entity      = $value->entity;
+		$this->entityClass = get_class($this->entity);
 
-		$count = 0;
-		foreach ($value->attributes as $attributeName) {
-			$attributeValue = $entity->get($attributeName);
+		return $this->chekInsertionScheduled() && $this->checkDatabase();
+
+
+	}
+
+	/**
+	 * Verifica se existe outras entidades iguais a esta na fila de entidades para inserção.
+	 * As entidades que estão agendadas para inserção, ainda não existem no banco de dados, por tanto, uma verificação
+	 * comum ao banco de dados não acusaria a duplicidade.
+	 *
+	 * Ex:
+	 *        // Cria duas entidades - assuma que elas estão duplicadas
+	 *        $e1 = new Entidade();
+	 *        $e1->setCpf('11111111111');
+	 *        $e2 = new Entidade();
+	 *        $e2->setCpf('11111111111');
+	 *        $em =  getEntityManager();
+	 *        $em->persist($e1);
+	 *        $em->persist($e2);
+	 *        $em->flush();
+	 *
+	 * No exemplo acima, as duas entidades duplicadas não existem no banco de dados, por tanto, para identificar esta
+	 * duplicidade é necessário verificar na fila de inserção.
+	 *
+	 * @return bool
+	 */
+	private function chekInsertionScheduled()
+	{
+		$uow                       = $this->em->getUnitOfWork();
+		$scheduledEntityInsertions = $uow->getScheduledEntityInsertions();
+		/** @var $sei AbstractEntity */
+		foreach ($scheduledEntityInsertions as $sei) {
+			if ($sei !== $this->entity && $sei instanceof $this->entityClass) {
+				$estaDuplicado = false;
+				foreach ($this->value->attributes as $attributeName) {
+					$attributeValue       = $this->entity->get($attributeName);
+					$otherEntityAttrValue = $sei->get($attributeName);
+					// @TODO Aprimorar a comparacao de campos dada/hora
+					// @TODO Aprimorar a comparacao de campos cujo valor é um objeto, mas não instancia de AbstractEntity
+					if ($attributeValue instanceof AbstractEntity) {
+						$estaDuplicado = ($attributeValue === $otherEntityAttrValue);
+					} else if ($attributeValue == $otherEntityAttrValue) {
+						$estaDuplicado = true;
+					} else {
+						$estaDuplicado = false;
+					}
+					
+					if (!$estaDuplicado) {
+						// Se pelo menos um dos atributos testados não estiver duplicado, então não podemos considerar
+						// a entidade como duplicada, e por isso nem precisamos continuar verificando os outros atributos.
+						break;
+					}
+				}
+
+				if ($estaDuplicado) {
+					$this->error(self::ENTIDADE_DUPLICADA);
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+
+	/**
+	 * Verifica duplicidade da entidade com os registros que já existem no banco de dados.
+	 *
+	 * @return bool
+	 */
+	private function checkDatabase()
+	{
+		$entityRepository = $this->em->getRepository($this->entityClass);
+		$qb               = $entityRepository->createQueryBuilder('a');
+		$count            = 0;
+		foreach ($this->value->attributes as $attributeName) {
+			$attributeValue = $this->entity->get($attributeName);
 
 			if ($attributeValue instanceof AbstractEntity) {
-				if (!$em->getRepository(get_class($attributeValue))->find((int)$attributeValue->getId())) {
+				if (!$this->em->getRepository(get_class($attributeValue))->find((int)$attributeValue->getId())) {
 					// Está verificando a duplicidade de valores em uma coluna FK (que aponta para outra tabela),
 					// porém o registro da outra tabela ainda não existe, então com certesa esta coluna não está duplicada.
 					// O regsitro da outra tabela pode não existir quando ainda não foi invocado EntityManager::flush().
@@ -54,13 +140,33 @@ class RegistroDuplicado extends \Zend\Validator\AbstractValidator
 			$qb->setParameter('attributeValue' . $count, $attributeValue);
 		}
 
-		if ($entity->getId()) {
-			$qb->andWhere('a.id <> :id');
-			$qb->setParameter('id', $entity->getId());
+		
+		$notInIds = array();
+		if ($this->entity->getId()) {
+			$notInIds[] = $this->entity->getId();
+		}
+
+		// Desconsidera as possiveis entidades duplicadas que foram agendadas para exlusão 
+		$scheduledEntityDeletions = $this->em->getUnitOfWork()->getScheduledEntityDeletions();
+		foreach ($scheduledEntityDeletions as $sed) {
+			if ($sed instanceof $this->entityClass) {
+				if ($sed->getId()) {
+					$notInIds[] = $sed->getId();
+				}
+			}
+		}
+		if ($notInIds) {
+			if (count($notInIds) == 1) {
+				$qb->andWhere($qb->expr()->neq('a.id', ':id'));
+				$notInIds = reset($notInIds);
+			} else {
+				$qb->andWhere($qb->expr()->notIn('a.id', ':id'));
+			}
+			$qb->setParameter('id', $notInIds);
 		}
 
 		$qb->setMaxResults(1);
-		
+
 		if ($qb->getQuery()->getOneOrNullResult() !== null) {
 			$this->error(self::ENTIDADE_DUPLICADA);
 			return false;
@@ -68,4 +174,5 @@ class RegistroDuplicado extends \Zend\Validator\AbstractValidator
 
 		return true;
 	}
+	
 }

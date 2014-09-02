7de822a1bdea811267dcb611ab560e79d9e61846
Stavarengo <faelsta@gmail.com>
2014-09-02 11:39:28 -0300

    AbstractActionExController.php

---
+++ src/Sta/Mvc/Controller/AbstractActionExController.php
@@ -0,0 +1,50 @@
+<?php
+
+
+namespace Sta\Mvc\Controller;
+
+
+use Doctrine\ORM\EntityManager;
+use Sta\Entity\AbstractEntity;
+use Zend\Mvc\Exception;
+use Zend\Mvc\MvcEvent;
+
+/**
+ * @author: Stavarengo
+ */
+class AbstractActionExController extends AbstractActionController
+{
+    /**
+     * Execute the request
+     *
+     * @param  MvcEvent $e
+     * @return mixed
+     * @throws Exception\DomainException
+     */
+    public function onDispatch(MvcEvent $e)
+    {
+        $routeMatch = $e->getRouteMatch();
+        if (!$routeMatch) {
+            return parent::onDispatch($e);
+        }
+
+        $action = $routeMatch->getParam('action', null);
+        
+        if (!$action) {
+            return parent::onDispatch($e);
+        }
+        
+        $method = static::getMethodFromAction($action);
+
+        if (!method_exists($this, $method)) {
+            return parent::onDispatch($e);
+        }
+
+        $actionResponse = Action::invoke($method);
+
+        $e->setResult($actionResponse);
+
+        return $actionResponse;
+    }
+    
+}

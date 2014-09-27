301b403a93e93fce07572ac368ffedc7adda978a
Stavarengo <faelsta@gmail.com>
2014-09-27 17:59:58 -0300

    criado opção para executar requsiição post com curl

--- src/Sta/Curl/Curl.php
+++ src/Sta/Curl/Curl.php
@@ -11,6 +11,23 @@ class Curl
 {
     public function getContent($url, array $params = array())
     {
+        return $this->_getContent($url, $params);
+    }
+
+    public function getContentByPost($url, array $postBody, array $params = array())
+    {
+        $params[CURLOPT_POST ] = true;
+        $params[CURLOPT_POSTFIELDS] = $postBody;
+        
+        return $this->_getContent($url, $params);
+    }
+    
+    /**
+     * @param array $params
+     * @return array
+     */
+    private function _getOptions(array $params)
+    {
         $options = array(
             CURLOPT_RETURNTRANSFER => true,
             CURLOPT_FOLLOWLOCATION => true,
@@ -22,16 +39,28 @@ class Curl
             $options[$k] = $v;
         }
 
+        return $options;
+    }
+
+    /**
+     * @param $url
+     * @param array $params
+     * @return mixed|null
+     */
+    private function _getContent($url, array $params)
+    {
+        $options = $this->_getOptions($params);
+
         $ch = curl_init($url);
         curl_setopt_array($ch, $options);
         $data = curl_exec($ch);
-        
+
         /* Check for 404 (file not found). */
         $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-        if($httpCode == 404) {
+        if ($httpCode == 404) {
             $data = null;
         }
-        
+
         curl_close($ch);
 
         return $data;

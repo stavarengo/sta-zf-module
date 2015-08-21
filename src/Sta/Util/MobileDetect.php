bc6d798b5842abd101adccaa12b4e073471cf178
Stavarengo <faelsta@gmail.com>
2015-08-21 15:56:43 -0300

    Workaroud to work with appcache

--- src/Sta/Util/MobileDetect.php
+++ src/Sta/Util/MobileDetect.php
@@ -13,6 +13,27 @@ class MobileDetect extends \Mobile_Detect
         if (!self::$instance) {
             self::$instance = new self();
         }
+
         return self::$instance;
     }
+
+    public function __construct(array $headers = null, $userAgent = null)
+    {
+        // Veja http://stackoverflow.com/a/32147456/2397394
+        $updateSession = false;
+        if ($userAgent === null) {
+            if (isset($_SESSION['\Sta\Util\MobileDetect::userAgent'])) {
+                $userAgent = $_SESSION['\Sta\Util\MobileDetect::userAgent'];
+            } else {
+                $updateSession = true;
+            }
+        }
+        
+        parent::__construct($headers, $userAgent);
+        
+        if ($updateSession) {
+            $_SESSION['\Sta\Util\MobileDetect::userAgent'] = $this->getUserAgent();
+        }
+    }
+
 } 

f17242fc977221bc989cafae7ced070500d0b128
Stavarengo <faelsta@gmail.com>
2015-11-24 16:13:28 -0200

    Craeted new param onn ExecPhpFileAsync

--- src/Sta/Cmd/ExecPhpFileAsync.php
+++ src/Sta/Cmd/ExecPhpFileAsync.php
@@ -3,23 +3,31 @@ namespace Sta\Cmd;
 
 /**
  *  Execute command via shell and return the complete output as a string.
+ *
  * @package Sta\Cmd
  */
 class ExecPhpFileAsync implements Command
 {
 
-	/**
-	 * @var string
-	 */
-	private $phpFile = null;
+    /**
+     * @var string
+     */
+    private $phpFile = null;
+    private $args = '';
 
-	public function __construct($phpFile)
-	{
-		$this->phpFile = (string)$phpFile;
-	}
+    public function __construct($phpFile, $args = '')
+    {
+        $this->phpFile = (string)$phpFile;
+        $this->args    = (string)$args;
+    }
 
-	public function execute()
-	{
-        return Invoker::invoke(new BackgroundExec('php -f "' . $this->phpFile . '"'));
-	}
+    public function execute()
+    {
+        $cmd = 'php -f "' . $this->phpFile . '"';
+        if ($this->args) {
+            $cmd .= ' -- ' . $this->args;
+        }
+
+        return Invoker::invoke(new BackgroundExec($cmd));
+    }
 }

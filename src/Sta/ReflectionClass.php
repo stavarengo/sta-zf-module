4ac9ee66e540b5e511e637c7679392f41ffc715c
Stavarengo <faelsta@gmail.com>
2016-04-19 11:29:54 -0300

    Updated ReflectionClass

--- src/Sta/ReflectionClass.php
+++ src/Sta/ReflectionClass.php
@@ -11,186 +11,199 @@ use ReflectionClass as PhpReflectionClass;
 class ReflectionClass
 {
 
-	/**
-	 * @var
-	 */
-	private static $_reader;
-	/**
-	 * @var array
-	 */
-	private static $_cache = array(
-		'self'     => array(),
-		'class'    => array(),
-		'property' => array(),
-		'method'   => array(),
-	);
-	/**
-	 * @var PhpReflectionClass
-	 */
-	private $className;
+    /**
+     * @var
+     */
+    private static $_reader;
+    /**
+     * @var array
+     */
+    private static $_cache = [
+        'self' => [],
+        'class' => [],
+        'property' => [],
+        'method' => [],
+    ];
+    protected $isDevelopment;
+    /**
+     * @var PhpReflectionClass
+     */
+    private $className;
 
-	protected function __construct($className)
-	{
-		$this->className = $className;
-	}
+    protected function __construct($className, $isDevelopment)
+    {
+        $this->className     = $className;
+        $this->isDevelopment = $isDevelopment;
+    }
 
-	/**
-	 * @param $objOrClassName
-	 *
-	 * @return ReflectionClass
-	 */
-	public static function factory($objOrClassName)
-	{
-		if (is_object($objOrClassName)) {
-			$objOrClassName = get_class($objOrClassName);
-		}
+    /**
+     * @param $objOrClassName
+     *
+     * @return ReflectionClass
+     */
+    public static function factory($objOrClassName, $isDevelopment)
+    {
+        if (is_object($objOrClassName)) {
+            $objOrClassName = get_class($objOrClassName);
+        }
 
-		if (!isset(self::$_cache['self'][$objOrClassName])) {
-			self::$_cache['self'][$objOrClassName] = new self($objOrClassName);
-		}
+        if (!isset(self::$_cache['self'][$objOrClassName])) {
+            self::$_cache['self'][$objOrClassName] = new self($objOrClassName, $isDevelopment);
+        }
 
-		return self::$_cache['self'][$objOrClassName];
-	}
+        return self::$_cache['self'][$objOrClassName];
+    }
 
-	/**
-	 * Gets the annotations applied to a class.
-	 *
-	 * @return Annotation[].
-	 */
-	public function getClassAnnotations()
-	{
-		return $this->getReader()->getClassAnnotations($this->getRefClass());
-	}
+    /**
+     * Gets the annotations applied to a class.
+     *
+     * @return Annotation[].
+     */
+    public function getClassAnnotations()
+    {
+        return $this->getReader()->getClassAnnotations($this->getRefClass());
+    }
 
-	/**
-	 * Gets a class annotation.
-	 *
-	 * @param string $annotationName
-	 *        The name of the annotation.
-	 *
-	 * @return Annotation
-	 *        The Annotation or NULL, if the requested annotation does not exist.
-	 */
-	public function getClassAnnotation($annotationName)
-	{
-		return $this->getReader()->getClassAnnotation($this->getRefClass(), $annotationName);
-	}
+    /**
+     * Gets a class annotation.
+     *
+     * @param string $annotationName
+     *        The name of the annotation.
+     *
+     * @return Annotation
+     *        The Annotation or NULL, if the requested annotation does not exist.
+     */
+    public function getClassAnnotation($annotationName)
+    {
+        return $this->getReader()->getClassAnnotation($this->getRefClass(), $annotationName);
+    }
 
-	/**
-	 * Gets the annotations applied to a property.
-	 *
-	 * @param string $property
-	 *            The property name from which the annotations should be read.
-	 *
-	 * @return Annotation[]
-	 */
-	public function getPropertyAnnotations($property)
-	{
-		return $this->getReader()->getPropertyAnnotations($this->getRefProperty($property));
-	}
+    /**
+     * Gets the annotations applied to a property.
+     *
+     * @param string $property
+     *            The property name from which the annotations should be read.
+     *
+     * @return Annotation[]
+     */
+    public function getPropertyAnnotations($property)
+    {
+        return $this->getReader()->getPropertyAnnotations($this->getRefProperty($property));
+    }
 
-	/**
-	 * Gets a property annotation.
-	 *
-	 * @param string $property
-	 * @param string $annotationName
-	 *        The name of the annotation.
-	 *
-	 * @return Annotation
-	 *        The Annotation or NULL, if the requested annotation does not exist.
-	 */
-	public function getPropertyAnnotation($property, $annotationName)
-	{
-		return $this->getReader()->getPropertyAnnotation($this->getRefProperty($property), $annotationName);
-	}
+    /**
+     * Gets a property annotation.
+     *
+     * @param string $property
+     * @param string $annotationName
+     *        The name of the annotation.
+     *
+     * @return Annotation
+     *        The Annotation or NULL, if the requested annotation does not exist.
+     */
+    public function getPropertyAnnotation($property, $annotationName)
+    {
+        return $this->getReader()->getPropertyAnnotation($this->getRefProperty($property), $annotationName);
+    }
 
-	/**
-	 * Gets the annotations applied to a method.
-	 *
-	 * @param string $method
-	 *        The name of the method from which the annotations should be read.
-	 *
-	 * @return Annotation[].
-	 */
-	public function getMethodAnnotations($method)
-	{
-		return $this->getReader()->getMethodAnnotations($this->getRefMethod($method));
-	}
+    /**
+     * Gets the annotations applied to a method.
+     *
+     * @param string $method
+     *        The name of the method from which the annotations should be read.
+     *
+     * @return Annotation[].
+     */
+    public function getMethodAnnotations($method)
+    {
+        return $this->getReader()->getMethodAnnotations($this->getRefMethod($method));
+    }
 
-	/**
-	 * Gets a method annotation.
-	 *
-	 * @param string $method
-	 * @param string $annotationName
-	 *        The name of the annotation.
-	 *
-	 * @return Annotation
-	 *        The Annotation or NULL, if the requested annotation does not exist.
-	 */
-	public function getMethodAnnotation($method, $annotationName)
-	{
-		return $this->getReader()->getMethodAnnotation($this->getRefMethod($method), $annotationName);
-	}
+    /**
+     * Gets a method annotation.
+     *
+     * @param string $method
+     * @param string $annotationName
+     *        The name of the annotation.
+     *
+     * @return Annotation
+     *        The Annotation or NULL, if the requested annotation does not exist.
+     */
+    public function getMethodAnnotation($method, $annotationName)
+    {
+        return $this->getReader()->getMethodAnnotation($this->getRefMethod($method), $annotationName);
+    }
 
     /**
      * @return PhpReflectionClass
      */
     public function getRefClass()
-	{
-		if (!isset(self::$_cache['class'][$this->className])) {
-			self::$_cache['class'][$this->className] = new PhpReflectionClass($this->className);
-		}
-		return self::$_cache['class'][$this->className];
-	}
+    {
+        if (!isset(self::$_cache['class'][$this->className])) {
+            self::$_cache['class'][$this->className] = new PhpReflectionClass($this->className);
+        }
+
+        return self::$_cache['class'][$this->className];
+    }
 
     /**
      * @param $property
+     *
      * @return \ReflectionProperty
      */
     public function getRefProperty($property)
-	{
-		if (!isset(self::$_cache['property'][$this->className][$property])) {
-			self::$_cache['property'][$this->className][$property] = new \ReflectionProperty($this->className, $property);
-		}
-		return self::$_cache['property'][$this->className][$property];
-	}
+    {
+        if (!isset(self::$_cache['property'][$this->className][$property])) {
+            self::$_cache['property'][$this->className][$property] = new \ReflectionProperty(
+                $this->className, $property
+            );
+        }
+
+        return self::$_cache['property'][$this->className][$property];
+    }
 
     /**
      * @param $method
+     *
      * @return \ReflectionMethod
      */
     public function getRefMethod($method)
-	{
-		if (!isset(self::$_cache['method'][$this->className][$method])) {
-			self::$_cache['method'][$this->className][$method] = new \ReflectionMethod($this->className, $method);
-		}
-		return self::$_cache['method'][$this->className][$method];
-	}
+    {
+        if (!isset(self::$_cache['method'][$this->className][$method])) {
+            self::$_cache['method'][$this->className][$method] = new \ReflectionMethod($this->className, $method);
+        }
+
+        return self::$_cache['method'][$this->className][$method];
+    }
 
     /**
      * @return Annotations\CachedReader
      */
     private function getReader()
-	{
-		if (!self::$_reader) {
-			self::$_reader = new Annotations\AnnotationReader;
-			$conf          = \Sta\Module::getServiceLocator()->get('Configuration');
-			$staConf       = $conf['sta'];
-            $cache = $staConf['ReflectionClass']['cache'];
+    {
+        if (!self::$_reader) {
+            self::$_reader = new Annotations\AnnotationReader;
+            $conf          = \Sta\Module::getServiceLocator()->get('Configuration');
+            $staConf       = $conf['sta'];
+            $cache         = $staConf['ReflectionClass']['cache'];
             if ($cache && !is_object($cache)) {
                 if ($cache == 'filesystem') {
-                    $cache = new \Doctrine\Common\Cache\FilesystemCache($staConf['ReflectionClass']['cacheDir'], 'sta.ref.class');
+                    $cache = new \Doctrine\Common\Cache\FilesystemCache(
+                        $staConf['ReflectionClass']['cacheDir'],
+                        'sta.ref.class'
+                    );
                 } else {
                     $cache = new $cache;
                 }
             }
             self::$_reader = new Annotations\CachedReader(
-				new Annotations\IndexedReader(self::$_reader),
+                new Annotations\IndexedReader(self::$_reader),
                 $cache,
-                \App\IsLocal::is()
-			);
-		}
-		return self::$_reader;
-	}
+                $this->isDevelopment
+            );
+        }
 
-} 
+        return self::$_reader;
+    }
+
+}

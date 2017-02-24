45b6c0d3c1b2374dca92eff4f027cf913f616c4e
Stavarengo <faelsta@gmail.com>
2017-02-24 11:28:02 -0300

    improved GetRequestContent

--- src/Sta/Mvc/Controller/Plugin/GetRequestContent.php
+++ src/Sta/Mvc/Controller/Plugin/GetRequestContent.php
@@ -2,7 +2,7 @@
 
 namespace Sta\Mvc\Controller\Plugin;
 
-use Zend\Http\PhpEnvironment\Request;
+use Sta\Mvc\Controller\AbstractActionController;
 use Zend\Mvc\Controller\Plugin\AbstractPlugin;
 
 /**
@@ -11,22 +11,23 @@ use Zend\Mvc\Controller\Plugin\AbstractPlugin;
 class GetRequestContent extends AbstractPlugin
 {
 
-	public function __invoke($objectDecodeType = \Zend\Json\Json::TYPE_ARRAY)
-	{
-		return $this->getRequestContent($objectDecodeType);
-	}
+    public function __invoke($objectDecodeType = \Zend\Json\Json::TYPE_ARRAY)
+    {
+        return $this->getRequestContent($objectDecodeType);
+    }
 
-	/**
-	 * Converte o conteúdo de uma requisição (POST ou PUT) para array, respeitando o parâmetro "format".
-	 *
-	 * @throws GetRequestContentException
-	 * @return array
-	 */
-	public function getRequestContent($objectDecodeType = \Zend\Json\Json::TYPE_ARRAY, $autoDetectSource = true)
-	{
-		/** @var $request Request */
-		$request = $this->getController()->getRequest();
-        
+    /**
+     * Converte o conteúdo de uma requisição (POST ou PUT) para array, respeitando o parâmetro "format".
+     *
+     * @throws GetRequestContentException
+     * @return array|object
+     */
+    public function getRequestContent($objectDecodeType = \Zend\Json\Json::TYPE_ARRAY, $autoDetectSource = true)
+    {
+        /** @var AbstractActionController $controller */
+        $controller = $this->getController();
+        $request    = $controller->getRequest();
+
         if ($autoDetectSource) {
             /** @var $contentType \Zend\Http\Header\ContentType */
             $contentType = $request->getHeader('ContentType');
@@ -35,65 +36,66 @@ class GetRequestContent extends AbstractPlugin
                 if ($objectDecodeType == \Zend\Json\Json::TYPE_OBJECT) {
                     $data = (object)$data;
                 }
-                
+
                 return $data;
             } else {
-                $data    = trim($request->getContent());
+                $data = trim($request->getContent());
             }
         } else {
-            $data    = trim($request->getContent());
+            $data = trim($request->getContent());
         }
-        
 
-		try {
-			$format      = $this->getController()->getParam('format', 'json');
-			$formatUpper = strtoupper($format);
-			if ($format == 'xml') {
-				@$data = \Zend\Json\Json::fromXml($data, false);
-			}
-			$data = \Zend\Json\Json::decode($data, $objectDecodeType);
-		} catch (\Exception $e) {
-			throw new GetRequestContentException("O conteúdo da requisição deve ser uma string $formatUpper válida.");
-		}
-		if (!$data) {
-			throw new GetRequestContentException("O conteúdo da requisição deve ser uma string $formatUpper válida.");
-		} else {
-			if ($format == 'xml') {
-				if (!isset($data['root'])) {
-					throw new GetRequestContentException('O XML recebido dever ter uma tag root com nome "root".');
-				}
-				$data = $this->normalizeAttributes($data['root']);
-			}
-		}
 
-		return $data;
-	}
+        $format      = $controller->getParam('format', 'json');
+        $formatUpper = strtoupper($format);
+        try {
+            if ($format == 'xml') {
+                @$data = \Zend\Json\Json::fromXml($data, false);
+            }
+            $data = \Zend\Json\Json::decode($data, $objectDecodeType);
+        } catch (\Exception $e) {
+            $data = null;
+        }
+        if (!$data) {
+            throw new GetRequestContentException("The content of the request must be a valid $formatUpper string.");
+        } else {
+            if ($format == 'xml') {
+                if (!isset($data['root'])) {
+                    throw new GetRequestContentException('The received XML must have a root tag named "root".');
+                }
+                $data = $this->normalizeAttributes($data['root']);
+            }
+        }
 
-	private function normalizeAttributes(array $data)
-	{
-		foreach ($data as $key => $value) {
-			if (is_array($value)) {
-				if (isset($value['@attributes']) && is_array($value['@attributes'])) {
-					// Verificamos quais dos atributos suportados foram usados, e depois disto ignora os demais atributos.
+        return $data;
+    }
 
-					$newValue = ''; // Por padrão os campos XML são string vazias
-					if (array_key_exists('@text', $value)) {
-						$newValue = $value['@text'];
-					}
+    private function normalizeAttributes(array $data)
+    {
+        foreach ($data as $key => $value) {
+            if (is_array($value)) {
+                if (isset($value['@attributes']) && is_array($value['@attributes'])) {
+                    // Verificamos quais dos atributos suportados foram usados, e depois disto ignora os demais atributos.
 
-					$attributes = $value['@attributes'];
-					if (array_key_exists('nil', $attributes) && $attributes['nil']) {
-						// Este campo é nulo
-						$newValue = null;
-					}
+                    $newValue = ''; // Por padrão os campos XML são string vazias
+                    if (array_key_exists('@text', $value)) {
+                        $newValue = $value['@text'];
+                    }
 
-					$data[$key] = $newValue;
-				} else {
-					// Este valor é um array, mas não é um array de atributos, então deve ser um array com mais dados de otura entidade
-					$data[$key] = $this->normalizeAttributes($value);
-				}
-			}
-		}
-		return $data;
-	}
+                    $attributes = $value['@attributes'];
+                    if (array_key_exists('nil', $attributes) && $attributes['nil']) {
+                        // Este campo é nulo
+                        $newValue = null;
+                    }
+
+                    $data[$key] = $newValue;
+                } else {
+                    // Este valor é um array, mas não é um array de atributos, então deve ser um array com mais dados de otura entidade
+                    $data[$key] = $this->normalizeAttributes($value);
+                }
+            }
+        }
+
+        return $data;
+    }
 }
